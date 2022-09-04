5715d97bb4491039bc87988e36afe2948ed2eb6d
sufiyan2401 <110626687+sufiyan2401@users.noreply.github.com>
2022-09-04 17:32:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+alert("Please Enter Number 100-105")
+let a=prompt("Enter your roll number")
+if(a == "100"){
+    alert ("You Got 90% ")
+}
+else if(a == "101"){
+    alert("You Got 95% ")
+}
+
+else if(a == "102"){
+    alert("You Got 85% ")
+}
+else if(a == "103"){
+    alert("You Got 87% ")
+}
+else if(a == "104"){
+    alert("You Got 82% ")
+}
+else if(a == "105"){
+    alert("You Got 80% ")
+}
+else if(a){
+    alert("You Inputed Wrong Value")
+}
+
+
